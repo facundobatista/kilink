09887e48793e14f286c1e833ea600731f06452b6
Nico Cesar <nico@netketing.com>
2011-11-11 09:51:27 -0300

    cambio el style

--- wsgi/hello_flup.py
+++ wsgi/hello_flup.py
@@ -20,7 +20,7 @@ MAIN_PAGE = """
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
     <title>Kilink</title>
 <style type="text/css">
-textarea#styleid{
+textarea#title{
 color:#666;
 font-size:14px;
 -moz-border-radius: 8px; -webkit-border-radius: 8px;
@@ -39,7 +39,7 @@ box-shadow: 0 0 5px rgba(81, 203, 238, 0);
 }
 
 
-textarea#styleid:focus{
+textarea#title:focus{
 color:#000;
 outline:none;
 border:#35a5e5 1px solid;
