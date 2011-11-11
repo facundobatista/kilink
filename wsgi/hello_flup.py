9bbcc88b67fb3033bb9b28edbedd6a70e15d7091
Facundo Batista <facundo@taniquetil.com.ar>
2011-11-11 00:47:44 -0300

    Cleaning!

--- wsgi/hello_flup.py
+++ wsgi/hello_flup.py
@@ -1,39 +1,21 @@
 #!/bin/env python
-"""
-Demo standalone Python FastCGI daemon using "flup" FastCGI->WSGI server
-behind Nginx.
 
-CentOS prerequisites -- from EPEL repository:
-yum install python-flup nginx
+"""Kilink FTW!"""
 
-1) Start this program and leave it running
-2) Copy "nginx.conf" in this directory to /etc/nginx, then: /etc/init.d/nginx start
-3) Try hitting any URL on http://localhost:88/
-
-References:
-http://trac.saddi.com/flup
-http://pypi.python.org/pypi/flup/
-http://wiki.codemongers.com/NginxModules
-"""
-from flup.server.fcgi import WSGIServer
-import time, os, sys
-import optparse
 import cgi
+import os
+import sys
 
 import backend
 
-__usage__ = "%prog -n <num>"
-__version__ = "$Id$"
-__author__ = "Matt Kangas <matt@daylife.com>"
+from flup.server.fcgi import WSGIServer
 
 #FCGI_LISTEN_ADDRESS = ('localhost', 9000)
 FCGI_SOCKET_DIR = '/tmp'
 FCGI_SOCKET_UMASK = 0111
 
 def myapp(environ, start_response):
-    """Dummy response handler"""
-    # SLEEP 100 ms to force a demostration of parallel threading performance
-    time.sleep(0.1)
+    """Generic handler."""
     start_response('200 OK', [('Content-Type', 'text/plain')])
     path_info = environ['PATH_INFO']
     query_string = environ['QUERY_STRING']
@@ -46,36 +28,16 @@ def myapp(environ, start_response):
     return [str(result)]
 
 
-def get_application():
-    return myapp
-
-def get_socketpath(name, server_number):
-    #return os.path.join(FCGI_SOCKET_DIR, 'fcgi-%s-%s.socket' % (name, server_number))
-    return os.path.join(FCGI_SOCKET_DIR, 'kilink.socket' )
-
 def main(args_in, app_name="hello"):
-    p = optparse.OptionParser(description=__doc__, version=__version__)
-    p.set_usage(__usage__)
-    p.add_option("-v", action="store_true", dest="verbose", help="verbose logging")
-    p.add_option("-n", type="int", dest="server_num", help="Server instance number")
-    opt, args = p.parse_args(args_in)
-
-    if not opt.server_num:
-        print "ERROR: server number not specified"
-        p.print_help()
-        return
-
-    socketfile = get_socketpath(app_name, opt.server_num)
-    app = get_application()
+    """Go girl."""
+    socketfile = os.path.join(FCGI_SOCKET_DIR, 'kilink.socket' )
 
     try:
-        WSGIServer(app,
-               bindAddress = socketfile,
-               umask = FCGI_SOCKET_UMASK,
-               multiplexed = True,
-               ).run()
+        srvr = WSGIServer(myapp, bindAddress=socketfile,
+                          umask=FCGI_SOCKET_UMASK, multiplexed=True)
+        srvr.run()
     finally:
-        # Clean up server socket file
+        # clean up server socket file
         os.unlink(socketfile)
 
 if __name__ == '__main__':
