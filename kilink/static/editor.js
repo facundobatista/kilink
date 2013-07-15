d2bd9add6cc237b31cf8e66555000d1da206ff0b
damianavila <damianavila82@yahoo.com.ar>
2013-07-15 19:25:09 -0300

    First working implementation of a hinghlight.js language detector.

--- kilink/static/editor.js
+++ kilink/static/editor.js
@@ -1,23 +1,21 @@
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-  mode: "python",
   lineNumbers: true,
   tabMode: "indent"
 });
 
 editor.setOption("theme", 'monokai');
 
-/*
-  editor.on("change", function() {
-    clearTimeout(pending);
-    setTimeout(update, 400);
-  });
+editor.on("change", function() {
+  clearTimeout(pending);
+  setTimeout(update, 400);
+});
 
-  var pending;
-  function looksLikeScheme(code) {
-    return !/^\s*\(\s*function\b/.test(code) && /^\s*[;\(]/.test(code);
-  }
+var pending;
+function looksLike(contents) {
+  info = hljs.highlightAuto(contents);
+  return info.language;
+}
 
-  function update() {
-    editor.setOption("mode", looksLikeScheme(editor.getValue()) ? "scheme" : "javascript");
-  }
-*/
+function update() {
+  editor.setOption("mode", looksLike(editor.getValue()));
+}
