2f9f5139ce5ad2effcd8f2150c503fbf10ecfe2b
damianavila <damianavila82@yahoo.com.ar>
2013-07-21 09:34:25 -0300

    Added logic in editor.js for language and theme selection.

--- kilink/static/editor.js
+++ kilink/static/editor.js
@@ -5,16 +5,57 @@ var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
 
 editor.setOption("theme", 'monokai');
 
+/* Theme selector */
+
+var input = document.getElementById("selectheme");
+function selectTheme() {
+  var theme = input.options[input.selectedIndex].innerHTML;
+  editor.setOption("theme", theme);
+  }
+
+/* Mode (language) selector */
+
+var modeInput = document.getElementById("seleclang");
+function selectMode() {
+  var mode = modeInput.options[modeInput.selectedIndex].innerHTML;
+  cmode = langLike(mode);
+  if (cmode=="auto")
+  {
+  autoDetection = 1;
+  update();
+  }
+  else
+  {
+  autoDetection = 0;
+  editor.setOption("mode", cmode);
+  modeInput.options[0].text = "auto";
+  }
+//  CodeMirror.autoLoadMode(editor, mode);
+  }
+
+/* Autodetector */
+
+var autoDetection = 1;
 editor.on("change", function() {
+  if (autoDetection){
   clearTimeout(pending);
   setTimeout(update, 400);
+  }
+  else
+  {
+//do nothing
+  }
 });
 
 var pending;
 function looksLike(contents) {
   info = hljs.highlightAuto(contents);
-  lang = info.language;
-  if (lang in {'cpp':0, 'cs':0, 'scala':0, 'java':0})
+  clang = langLike(info.language)
+  return clang;
+}
+
+function langLike(lang) {
+  if (lang in {'cpp':0, 'c++':0, 'cs':0, 'c#':0, 'scala':0, 'java':0})
     {
   lang = "clike";
     }
@@ -25,18 +66,28 @@ function looksLike(contents) {
   else if (lang=="json")
     {
   lang = "javascript";
-    }        
+    }
   else if (lang=="tex")
     {
   lang = "stex";
-    }       
+    }
+  else if (lang in {'1c':0, 'avr':0, 'assembler':0, 'actionscript':0,
+        'apache':0, 'applescript':0, 'axapta':0, 'bash':0, 'brainfuck':0,
+        'cmake':0, 'dos':0, '.bat':0, 'delphi':0, 'django':0, 'glsl':0,
+        'ini':0, 'lisp':0, 'mel':0, 'matlab':0, 'nginx':0, 'objectivec':0,
+        'parser3':0, 'profile':0, 'rsl':0, 'rib':0, 'vhdl':0, 'vala':0})
+    {
+  lang = "undefined";
+    }
   else
     {
-//  do nothing    
+//  do nothing
     }
   return lang;
 }
 
 function update() {
-  editor.setOption("mode", looksLike(editor.getValue()));
+  var langMode = looksLike(editor.getValue());
+  editor.setOption("mode", langMode);
+  modeInput.options[modeInput.selectedIndex].text = "auto: " + langMode;
 }
