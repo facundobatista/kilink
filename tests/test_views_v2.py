7f8ada02899ed01a6ea54b238d44984a19a695f8
Sofia Denner <sofi.denner@gmail.com>
2024-06-22 20:26:26 -0300

    add tests for get linkode api

--- tests/test_views_v2.py
+++ tests/test_views_v2.py
@@ -1,6 +1,4 @@
 """API v2 tests."""
-import json
-
 from kilink.backend import kilinkbackend, KilinkDataTooBigError
 from kilink.config import config, UNITTESTING_ENVIRONMENT_VALUE
 from kilink.main import app
@@ -20,31 +18,27 @@ class TestCreateLinkode:
 
     def test_creates_new_root_with_all_params(self):
         payload = {
-            "content": "print('hola mundo')",
+            "content": "print('hello world')",
             "text_type": "python",
         }
         response = test_client.post("/api/2/linkode/", json=payload)
 
-        content = json.loads(response.data)
-
         for key in self.EXPECTED_KEYS_IN_RESPONSE:
-            assert key in content
+            assert key in response.json
 
         assert response.status_code == 201
 
         # created linkode is the root of its tree
-        assert content["linkode_id"] == content["root_id"]
+        assert response.json["linkode_id"] == response.json["root_id"]
 
     def test_creates_new_root_without_text_type(self):
         payload = {
-            "content": "print('hola mundo')",
+            "content": "print('hello world')",
         }
         response = test_client.post("/api/2/linkode/", json=payload)
 
-        content = json.loads(response.data)
-
         for key in self.EXPECTED_KEYS_IN_RESPONSE:
-            assert key in content
+            assert key in response.json
 
         assert response.status_code == 201
 
@@ -52,23 +46,24 @@ class TestCreateLinkode:
         parent_id = kilinkbackend.create_linkode(content="", text_type="").linkode_id
 
         payload = {
-            "content": "print('hola mundo')",
+            "content": "print('hello world')",
             "text_type": "python",
         }
         response = test_client.post(f"/api/2/linkode/{parent_id}/", json=payload)
-        content = json.loads(response.data)
 
         for key in self.EXPECTED_KEYS_IN_RESPONSE:
-            assert key in content
+            assert key in response.json
 
         assert response.status_code == 201
 
         # new linkoded created with id different than its parent
-        assert content["linkode_id"] != parent_id
-        assert content["root_id"] == parent_id
+        assert response.json["linkode_id"] != parent_id
+        assert response.json["root_id"] == parent_id
 
     def test_creates_new_revision_when_parent_not_found(self):
-        response = test_client.post("/api/2/linkode/someNonExistingLinkodeId/", json={"content": "x"})
+        response = test_client.post(
+            "/api/2/linkode/someNonExistingLinkodeId/", json={"content": "x"}
+        )
 
         assert response.status_code == 404
 
@@ -86,3 +81,24 @@ class TestCreateLinkode:
 
         assert response.status_code == 413
         assert "too big" in str(response.data)
+
+
+class TestGetLinkode:
+    def test_get_linkode_happy_path(self):
+        linkode = kilinkbackend.create_linkode(content="print('hello world')", text_type="python")
+
+        response = test_client.get(f"/api/2/linkode/{linkode.linkode_id}")
+
+        assert response.status_code == 200
+        assert response.json["content"] == "print('hello world')"
+        assert response.json["text_type"] == "python"
+        assert response.json["linkode_id"] == linkode.linkode_id
+        assert response.json["root_id"] == linkode.root
+        assert "timestamp" in response.json
+        assert "linkode_url" in response.json
+        assert "root_id" in response.json
+
+    def test_get_linkode_when_not_found(self):
+        response = test_client.get("/api/2/linkode/SomeUnexistingId/")
+
+        assert response.status_code == 404
