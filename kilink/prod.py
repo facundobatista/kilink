18a47cc6cfc40a88344ad11ea3942c45e4d040b6
Facundo Batista <facundo@taniquetil.com.ar>
2024-09-07 15:08:32 -0300

    Details to enable prod deployment.

--- kilink/prod.py
+++ kilink/prod.py
@@ -1,9 +1,3 @@
-import sys
-
-sys.path.insert(0, '/home/kilink/.virtualenvs/kilink/lib/python3.8/site-packages')
-sys.path.insert(0, "/home/kilink/project/production/")
-
-
 from kilink import main, loghelper
 from kilink.config import config
 
